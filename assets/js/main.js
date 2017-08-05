463f12a078f5326638aa1c9f362177bb72f85925
DianaQuispe <danaalienmas@gmail.com>
2017-08-05 12:31:13 -0500

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,16 +19,17 @@ for(var i =0; i<mapa.length; i++) {
 	var fila = document.createElement("tr");
 	for(var j=0; j<mapa[i].length; j++) {
 		var celda = document.createElement("td");
+      //    celda.classList.toggle("todas");
 	//	var textoCelda = document.createTextNode(mapa[i][j]);
      //	celda.appendChild(textoCelda);
      		if(mapa[i][j] == "o" ) {
-	     		celda.setAttribute("class", "inicio")
+	     		celda.setAttribute("class", "inicio");
      		}else if(mapa[i][j] == "W") {
-   				celda.setAttribute("class", "final")
+   				celda.setAttribute("class", "final");
      		}else if(mapa[i][j] == "*") {
-     			celda.setAttribute("class", "asterisco")
+     			celda.setAttribute("class", "asterisco");
      		}else if(mapa[i][j] == "_") {
-     			celda.setAttribute("class", "subguion")
+     			celda.setAttribute("class", "subguion");
      		}		
      		fila.appendChild(celda);
 
@@ -42,3 +43,23 @@ for(var i =0; i<mapa.length; i++) {
 			tablero.appendChild(tabla);
 	}
 
+var left = document.getElementById('left');
+left.onclick = function() {
+  var celda = document.getElementsByTagName("td");
+     for(var i =0; i <celda.length; i++) {
+        celda[i].setAttribute("class", "todas");
+  
+        //  celda[i].style.background = "url('assets/imagenes/up.png')";
+     }
+     alert("sera");
+}
+
+var right = document.getElementById('right');
+right.onclick = function() {
+     alert("sera");
+}
+
+var forward = document.getElementById('forward');
+forward.onclick = function() {
+     alert("sforwardera");
+}
