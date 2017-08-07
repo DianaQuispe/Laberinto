8e03bcae778cd6dad6b5b5963de2e7eb7656dced
DianaQuispe <danaalienmas@gmail.com>
2017-08-07 17:33:19 -0500

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var down = document.getElementById("down");
 var left = document.getElementById("left");
 var right = document.getElementById("right");
 var forward = document.getElementById('forward');
-
+var restart = document.getElementById("restart");
 tabla.setAttribute('id','tabla');
 tabla.setAttribute = ("class", "tablita");
 var x,y;
@@ -13,7 +13,7 @@ var x,y;
 var mapa=[
 "******************",
 "*_________*______*",
-"*_*****_____******",
+"*_****______******",
 "*______***__*__*_*",
 "***_*____*____**_*",
 "*___*____**__*___*",
@@ -29,10 +29,11 @@ for(var i =0; i<mapa.length; i++) {
         var celda = document.createElement("td");
             if(mapa[i][j] == "o" ) {
                 celda.setAttribute("class", "inicio");
+                celda.setAttribute("id","posi");
                 x=i;
                 y=j;
-                console.log(x);
-                console.log(y);
+                console.log(x +"," + y);
+                
             }else if(mapa[i][j] == "W") {
                 celda.setAttribute("class", "final");
             }else if(mapa[i][j] == "*") {
@@ -42,47 +43,68 @@ for(var i =0; i<mapa.length; i++) {
             }       
             fila.appendChild(celda);
         } 
-            tabla.appendChild(fila);
-            tablero.appendChild(tabla);
+          tabla.appendChild(fila);
+          tablero.appendChild(tabla);
 }
-var tabla2 = document.getElementById("tabla");
-
-up.onclick = function() {
-    if (x>1 && mapa[x-1][y] != "*") {
-         tabla2.rows[x]  .cells[y].setAttribute("class", "subguion");
+up.onclick = function() { // X  ^ disminuye y Y -> se mantiene
+    if (x >1 && mapa[x-1][y] != "*") {
+     console.log(x +"," + y);;
+         tabla.rows[x] .cells[y].setAttribute("class", "subguion");
           x--;
-         tabla2.rows[x].cells[y].setAttribute("class", "up");
+         tabla.rows[x].cells[y].setAttribute("class", "up");
      }
 }
 
-left.onclick = function() {
+left.onclick = function() { // X ^ se mantiene Y disminuye ->
+console.log(x +"," + y);
      if (y>1 &&  mapa[x][y-1] != "*") {
-         tabla2.rows[x].cells[y].setAttribute("class", "subguion");
+         tabla.rows[x].cells[y].setAttribute("class", "subguion");
          y--; 
-         tabla2.rows[x].cells[y].setAttribute("class", "left");
+         tabla.rows[x].cells[y].setAttribute("class", "left");
       }   
 }
 
-right.onclick = function() {
+right.onclick = function() { //X  ^ se mantiene y Y aumenta ->
+     console.log(x +"," + y);
      if (y<16 && mapa[x][y+1] != "*") {
-         tabla2.rows[x].cells[y].setAttribute("class", "subguion");
+         tabla.rows[x].cells[y].setAttribute("class", "subguion");
          y++;
-         tabla2.rows[x].cells[y].setAttribute("class", "right");
+         tabla.rows[x].cells[y].setAttribute("class", "right");
      }
 }
 
-down.onclick = function() {
+down.onclick = function() { //X ^ aumenta y Y se mantiene ->
+     console.log(x +"," + y);
      if (x<9 &&  mapa[x+1][y] != "*") {
-         tabla2.rows[x].cells[y].setAttribute("class", "subguion");
+         tabla.rows[x].cells[y].setAttribute("class", "subguion");
          x++;
-         tabla2.rows[x].cells[y].setAttribute("class", "down");
+         tabla.rows[x].cells[y].setAttribute("class", "down");
      }
 }
 
-forward.onmouseup  = function() {
-     if (x>1 && mapa[x-1][y] != "*") {
-         tabla2.rows[x]  .cells[y].setAttribute("class", "subguion");
-          x--;
-        tabla2.rows[x].cells[y].setAttribute("class", "up");
-     }
+
+forward.onclick = function() {
+     myFunction();  
+   console.log(x +"," + y);
+
+      
+
 }
+function myFunction() {
+
+    setInterval(function(){ 
+     //var posi = document.getElementById("posi");
+    var t = true;
+     if(mapa[x][y] != "*" ) {
+        tabla.rows[x].cells[y].setAttribute("class", "up");
+          x--;   
+     }else if(tabla.rows[x].cells[y+1].setAttribute("class", "subguion")); {
+          tabla.rows[x].cells[y].setAttribute("class", "right"); 
+          y++;    }
+     }, 400);
+} 
+
+restart.onclick = function( ) {
+
+}
+  
